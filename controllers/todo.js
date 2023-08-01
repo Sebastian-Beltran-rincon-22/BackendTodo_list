ff6db63de63a3414321d7085a2f640e09e40165b
sewastian <sebastianrincon834@gmail.com>
2023-08-01 12:57:28 -0500

    update [controllers -> todo.js]

---
+++ controllers/todo.js
@@ -0,0 +1,69 @@
+const Todo = require(`../models/todo`)
+
+const todoControllers = {
+create: async (req,res) => {
+  try {
+      const todo = req.body.todo
+      await Todo.create ({
+        todo : todo
+      })
+      res.json({msg:'Created'})
+
+    } catch (error) {
+      return res.status(500).json ({msg:error.message})
+    }
+
+  },
+
+get: async (req,res) => {
+  try {
+      const todo = await Todo.find({})
+
+      res.json(
+        todo
+        )
+    } catch (error) {
+      return res.status(500).json({msg:error.message})
+    }
+  },
+
+getById: async (req,res) => {
+  try {
+        const {id} = req.params
+        const todo = await Todo.findById(id)
+        res.json(
+          todo
+          )
+    } catch (error) {
+        return res.status(500).json({msg:error.message})
+    }
+},
+
+update: async (req,res) =>{
+  try{
+      const {id} = req.params
+      const todo = req.body.todo
+      await Todo.findByIdAndUpdate(id,{
+        todo : todo
+      })
+      res.json({msg:'Updated'})
+  }catch(error){
+      console.error(error)
+      return res.status(500).json({msg:error.message})
+  }
+},
+
+delete: async (req,res)=>{
+  try {
+      const {id} = req.params
+      await Todo.findByIdAndDelete(id)
+      res.json({msg:'Deleted'})
+  } catch (err) {
+      console.error(err)
+      return res.status(500).json({msg:err.message})
+  }
+},
+
+}
+
+module.exports = todoControllers
